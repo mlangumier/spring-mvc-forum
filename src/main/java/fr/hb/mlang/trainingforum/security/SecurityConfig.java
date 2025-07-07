ff91eb06ee703d12138229cc1a183d9868edb5b4
Mathieu Langumier <mathieu.langumier@gmail.com>
2025-07-07 11:13:43 +0200

    Update: Changed our SecurityConfig to open routes by default and require auth on specific ones

--- src/main/java/fr/hb/mlang/trainingforum/security/SecurityConfig.java
+++ src/main/java/fr/hb/mlang/trainingforum/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package fr.hb.mlang.trainingforum.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,9 +15,14 @@ public class SecurityConfig {
   public SecurityFilterChain accessControl(HttpSecurity http) throws Exception {
     return http
         .authorizeHttpRequests(authorize -> authorize
-            .requestMatchers("/", "/login", "/register", "/author/*", "/styles/**").permitAll()
-            .anyRequest().authenticated()
+            // We set specific routes to require authentication
+            .requestMatchers("/new-post").authenticated()
+            // We can set specific CRUD operations to required authentication
+            .requestMatchers(HttpMethod.POST, "/post/**").authenticated()
+            // The rest of our routes are public
+            .anyRequest().permitAll()
         )
+        // Replace the default Spring Security login page with our own
         .formLogin(formLogin -> formLogin
             .loginPage("/login").permitAll()
             .defaultSuccessUrl("/", true)
